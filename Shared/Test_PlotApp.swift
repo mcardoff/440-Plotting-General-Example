eaa9ccce708953486030c4c4b24765d4c913c17c
Jeff Terry <terryj@iit.edu>
2021-01-25 21:36:52 -0600

    Initial Commit

---
+++ Shared/Test_PlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_PlotApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_PlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
