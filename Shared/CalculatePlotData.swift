98781c7411bc401d1b5bd7436f112b94e75dea8d
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-04 17:18:14 -0600

    Changed plotting func

--- Shared/CalculatePlotData.swift
+++ Shared/CalculatePlotData.swift
@@ -14,7 +14,7 @@ class CalculatePlotData: ObservableObject {
     var plotDataModel: PlotDataClass? = nil
     var theText = ""
     
-
+    
     @MainActor func setThePlotParameters(color: String, xLabel: String, yLabel: String, title: String) {
         //set the Plot Parameters
         plotDataModel!.changingPlotParameters.yMax = 10.0
@@ -40,64 +40,30 @@ class CalculatePlotData: ObservableObject {
         plotDataModel!.appendData(dataPoint: plotData)
     }
     
-    func plotYEqualsX() async
-    {
-        
-        theText = "y = x\n"
-        
-        await setThePlotParameters(color: "Red", xLabel: "x", yLabel: "y", title: "y = x")
-        
-        await resetCalculatedTextOnMainThread()
-        
-        
-        var plotData :[plotDataType] =  []
-        
-        
-        for i in 0 ..< 120 {
-             
-            //create x values here
-
-            let x = -2.0 + Double(i) * 0.2
-
-        //create y values here
-
-        let y = x
-
-
-            let dataPoint: plotDataType = [.X: x, .Y: y]
-            plotData.append(contentsOf: [dataPoint])
-            theText += "x = \(x), y = \(y)\n"
-        
-        }
-        
-        await appendDataToPlot(plotData: plotData)
-        await updateCalculatedTextOnMainThread(theText: theText)
-        
-        
+    func plotYEqualsX() async {
+        await plotFunc(fun: {(x:Double) -> Double in return x}, funcName: "y = x", color: "Blue")
     }
     
     
-    func ploteToTheMinusX() async
-    {
+    func ploteToTheMinusX() async {
+        await plotFunc(fun: {(x:Double) -> (Double) in return exp(-x)}, funcName: "y = exp(-x)", color: "Red")
+        return
+    }
+    
+    func plotFunc(fun: ((Double) -> Double), funcName: String, color: String) async {
         
         //set the Plot Parameters
-        
-        await setThePlotParameters(color: "Blue", xLabel: "x", yLabel: "y = exp(-x)", title: "y = exp(-x)")
-        
+        await setThePlotParameters(color: color, xLabel: "x", yLabel: funcName, title: funcName)
         await resetCalculatedTextOnMainThread()
-        
-        theText = "y = exp(-x)\n"
         
         var plotData :[plotDataType] =  []
         for i in 0 ..< 60 {
 
             //create x values here
-
             let x = -2.0 + Double(i) * 0.2
 
-        //create y values here
-
-        let y = exp(-x)
+            //create y values here
+            let y = fun(x)
             
             let dataPoint: plotDataType = [.X: x, .Y: y]
             plotData.append(contentsOf: [dataPoint])
@@ -111,17 +77,17 @@ class CalculatePlotData: ObservableObject {
     }
     
     
-        @MainActor func resetCalculatedTextOnMainThread() {
-            //Print Header
-            plotDataModel!.calculatedText = ""
+    @MainActor func resetCalculatedTextOnMainThread() {
+        //Print Header
+        plotDataModel!.calculatedText = ""
+        
+    }
     
-        }
     
-    
-        @MainActor func updateCalculatedTextOnMainThread(theText: String) {
-            //Print Header
-            plotDataModel!.calculatedText += theText
-        }
+    @MainActor func updateCalculatedTextOnMainThread(theText: String) {
+        //Print Header
+        plotDataModel!.calculatedText += theText
+    }
     
 }
 
